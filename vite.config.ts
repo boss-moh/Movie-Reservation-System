e70baa0e2e468556070f7b8862f3ab92cd5f6a4b
boss-moh <moh.saad.abu.kmail@gmail.com>
2026-04-13 12:14:48 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  
-  
+  resolve: {
+    tsconfigPaths: true
+  },
+
   test: {
     globals: true, // Allows using 'describe', 'it' without importing
     environment: "node", // Ensures Vitest doesn't try to use jsdom
