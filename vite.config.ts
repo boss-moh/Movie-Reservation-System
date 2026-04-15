91b514ba2f8df3ae48bef377f95646d5acf01ad3
boss-moh <moh.saad.abu.kmail@gmail.com>
2026-04-15 16:32:42 +0300

    Update the DB schema (#9)
    Add files relate for deleting process
    updated ignore file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true
   },
+  
 
   test: {
     globals: true, // Allows using 'describe', 'it' without importing
     environment: "node", // Ensures Vitest doesn't try to use jsdom
     setupFiles: ['./src/test/index.ts'],
+    server:{
+      deps: {
+        inline: ['generated/prisma'] 
+        }
+    }
   },
 });
