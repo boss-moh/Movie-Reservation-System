012351a8ab0f355bde42882a3f7f8fe0c6dc02ac
Mohammed Abu Kmail <99874218+boss-moh@users.noreply.github.com>
2026-05-25 14:31:59 +0300

    define types for response and error that return from our RESTAPI (#15)

--- src/modules/auth/controllers.ts
+++ src/modules/auth/controllers.ts
@@ -1,22 +1,44 @@
 import { Request, Response } from 'express';
-import * as AuthService from './services';
-import { RequestWithUser } from '@/types';
+import * as AuthService from '@/modules/auth/services';
+import { RequestWithUser, ApiResponse, userDTO } from '@/types';
+import { accessToken, AuthResponseDTO } from '@/modules/auth/type';
 
 
-export const registerController = async (req: Request, res: Response) => {
+export const registerController = async (req: Request, res: Response<ApiResponse<userDTO>>) => {
     const user = await AuthService.register(req.body);
-    res.status(201).json(user);
+    res.status(201).json({
+        response: {
+            status: 201,
+            message: 'User registered successfully',
+            success: true,
+            data: user,
+        },
+    });
 
 };
 
-export const loginController = async (req: Request, res: Response) => {
+export const loginController = async (req: Request, res: Response<ApiResponse<AuthResponseDTO>>) => {
     const result = await AuthService.login(req.body);
-    res.status(200).json(result);
+    res.status(200).json({
+        response: {
+            status: 200,
+            data: result,
+            message: 'Login successful',
+            success: true,
+        },
+    });
 
 };
 
-export const refreshTokenController = async (req: RequestWithUser, res: Response) => {
+export const refreshTokenController = async (req: RequestWithUser, res: Response<ApiResponse<accessToken>>) => {
     const { refreshToken = '' } = req.body
     const result = await AuthService.refreshAccessToken(refreshToken);
-    res.status(200).json(result);
+    res.status(200).json({
+        response: {
+            status: 200,
+            data: result,
+            message: 'Token refreshed successfully',
+            success: true,
+        },
+    });
 };
